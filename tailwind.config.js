e5d92d1af67c6836f82865e483888f3d9d22cb6b
Sindhu S <sindhusugumar80@gmail.com>
2025-10-29 09:07:46 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: { extend: {} },
+  plugins: [],
+};
+// Define your images array
+export const images = [
+  "/src/assests/images/l1.jpg",
+  "/src/assets/images/bird-cage.jpg",
+  "/src/assets/images/rabbit-hutch.jpg",
+];
+
